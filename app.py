0a6bad66acc51db2a53aa080a5352ce6dd0dcae5
ElizabethBarraza <elibarraza00@outlook.com>
2026-03-06 10:32:04 -0700

    Update 2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Modificacion para ver cambios en Render"
+    return "Modificacion 2 para ver cambios en Render"
 
 if __name__ == "__main__":
     app.run()
